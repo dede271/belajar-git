4113604498b9c9f6a57fd9311608cd33531d11b7
dede271 <156868805+dede271@users.noreply.github.com>
2024-01-17 09:30:12 +0700

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,8 @@
+nama = input("masukan nama : ")
+umur = input("masukan umur : ")
+alamat = input("masukan alamat : ")
+
+print("\n---Biodata---")
+print(" nama : " + nama)
+print(" umur : " + umur)
+print(" alamat : " + alamat)
