c78a153bceb982eed31cc83e4563b769e2533817
Dede Rahman <rahmandede271@gmail.com>
2024-01-17 09:45:13 +0700

    menambahkan kode input jurusan

--- biodata.py
+++ biodata.py
@@ -1,8 +1,10 @@
 nama = input("masukan nama : ")
 umur = input("masukan umur : ")
 alamat = input("masukan alamat : ")
+jurusan = input("masukan jurusan : ")
 
 print("\n---Biodata---")
 print(" nama : " + nama)
 print(" umur : " + umur)
 print(" alamat : " + alamat)
+print(" jurusan : " + jurusan)
